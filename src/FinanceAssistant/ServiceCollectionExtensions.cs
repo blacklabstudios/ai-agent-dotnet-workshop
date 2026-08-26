f7c39761c24d014a279fe354242af10006c836cc
Guilherme Ferreira <guilherme.as.ferreira@gmail.com>
2026-08-26 12:24:33 +0100

    P2.01: currency conversion tool

--- src/FinanceAssistant/ServiceCollectionExtensions.cs
+++ src/FinanceAssistant/ServiceCollectionExtensions.cs
@@ -29,6 +29,7 @@ public static class ServiceCollectionExtensions
                 .AsBuilder()
                 .ConfigureOptions(o =>
                     o.Reasoning = new ReasoningOptions { Effort = ReasoningEffort.None })
+                .UseFunctionInvocation()
                 .Build());
     }
 }
